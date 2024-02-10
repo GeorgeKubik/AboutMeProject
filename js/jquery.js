bd19bc76276d29fac68a561bc11fed8996c41d99
Георгий <59421323+GeorgeKubik@users.noreply.github.com>
2024-02-10 16:37:58 +0300

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+	'use strict';
+
+	const $navigationList = $('.navigation__list');
+	const $mainContent = $('.main__content');
+
+	async function viewHtmlContent(url) {
+		try {
+			const response = await fetch(url);
+			const html = await response.text();
+			$mainContent.html(html);
+			const $btn = $mainContent.find('.main__btn');
+			const $cardsSkills = $mainContent.find('.main__skills-card');
+			const $cardsExperience = $mainContent.find('.main__experience-card');
+			getElementContent($btn, $cardsSkills, $cardsExperience);
+
+		} catch (error) {
+			console.log('Ошибка при загрузке содержимого:', error);
+		}
+	}
+	viewHtmlContent('../about.html');
+
+	function hideContent($content) {
+		$content.each((i, item) => {
+			if (i >= 2) $(item).css('display', 'none');
+		});
+	}
+
+	function showContent($content) {
+		$content.css('display', 'block');
+	}
+
+	function changeContentBtn($btn) {
+		if ($btn.text() === 'Показать полностью') {
+			$btn.text('Свернуть');
+		} else {
+			$btn.text('Показать полностью');
+		}
+	}
+
+	function getElementContent($btn, $skills, $experience) {
+		const state = {
+			skills: false,
+			experience: false,
+		};
+
+		hideContent($skills);
+		hideContent($experience);
+
+		if ($btn.length === 0) return;
+
+		$btn.on('click', function () {
+			let $target = $(this);
+			if ($target.hasClass('btn-experience')) {
+				if (state.experience) {
+					hideContent($experience);
+					changeContentBtn($btn);
+					checkHightContent();
+				} else {
+					showContent($experience);
+					changeContentBtn($btn);
+					checkHightContent();
+				}
+				state.experience = !state.experience;
+			} else if ($target.hasClass('btn-skills')) {
+				if (state.skills) {
+					hideContent($skills);
+					changeContentBtn($btn);
+					checkHightContent();
+				} else {
+					showContent($skills);
+					changeContentBtn($btn);
+					checkHightContent();
+				}
+				state.skills = !state.skills;
+			}
+		});
+	}
+
+	$navigationList.on('click', '.navigation__link', function () {
+		let $target = $(this);
+		$('.navigation__link').removeClass('navigation__link_bold');
+		$target.addClass('navigation__link_bold');
+
+		if ($target.hasClass('about')) {
+			viewHtmlContent('../about.html');
+		} else if ($target.hasClass('experience')) {
+			viewHtmlContent('../experience.html');
+		} else if ($target.hasClass('skills')) {
+			viewHtmlContent('../skills.html');
+		}
+	});
+
+	function checkHightContent() {
+		if ($mainContent.outerHeight() >= 834) {
+			$mainContent.css('paddingBottom', '30px');
+		} else {
+			$mainContent.css('paddingBottom', '0px');
+		}
+	}
+	checkHightContent();
+});
---
+++ js/script.js
@@ -0,0 +1,112 @@
+window.addEventListener('DOMContentLoaded', () => {
+	'use strict';
+
+	const navigationList = document.querySelector('.navigation__list');
+	const navigationItems = document.querySelectorAll('.navigation__link')
+	const mainContent = document.querySelector('.main__content');
+
+	// Main content
+	async function viewHtmlContent(url) {
+		try {
+			const response = await fetch(url);
+			const html = await response.text();
+			mainContent.innerHTML = html;
+			const btn = mainContent.querySelector('.main__btn');
+			const cardsSkills = mainContent.querySelectorAll('.main__skills-card');
+			const cardsExperience = mainContent.querySelectorAll('.main__experience-card');
+			getElementContent(btn, cardsSkills, cardsExperience, mainContent);
+
+		} catch (error) {
+			console.log('Ошибка при загрузке содержимого:', error);
+		}
+	}
+	viewHtmlContent('../about.html');
+
+
+
+	function hideContent(content) {
+		Array.from(content).forEach((item, i) => {
+			if (i >= 2) item.style.display = 'none';
+		});
+	}
+
+	function showContent(content) {
+		Array.from(content).forEach(item => item.style.display = 'block');
+	}
+
+	function changeContentBtn(btnText) {
+		if (btnText.textContent === 'Показать полностью') {
+			btnText.textContent = 'Свернуть';
+		} else {
+			btnText.textContent = 'Показать полностью';
+		}
+	}
+
+	function getElementContent(btn, skills, experience, mainContent) {
+		const state = {
+			skills: false,
+			experience: false,
+		};
+
+		hideContent(skills);
+		hideContent(experience);
+
+		if (btn === null) return;
+
+		btn.addEventListener('click', (e) => {
+			let target = e.target;
+			if (target && target.classList.contains('btn-experience')) {
+				if (state.experience) {
+					hideContent(experience);
+					changeContentBtn(btn);
+					checkHightContent(mainContent);
+				} else {
+					showContent(experience);
+					changeContentBtn(btn);
+					checkHightContent(mainContent);
+				}
+				state.experience = !state.experience;
+			} else if (target && target.classList.contains('btn-skills')) {
+				if (state.skills) {
+					hideContent(skills);
+					changeContentBtn(btn);
+					checkHightContent(mainContent);
+				} else {
+					showContent(skills);
+					changeContentBtn(btn);
+					checkHightContent(mainContent);
+				}
+				state.skills = !state.skills;
+			}
+		});
+	}
+
+	navigationList.addEventListener('click', (e) => {
+		const target = e.target;
+
+		if (target && target.classList.contains('navigation__link')) {
+			navigationItems.forEach(item => item.classList.remove('navigation__link_bold'))
+			target.classList.add('navigation__link_bold');
+		}
+
+		if (target && target.classList.contains('about')) {
+			viewHtmlContent('../about.html');
+		} else if (target && target.classList.contains('experience')) {
+			viewHtmlContent('../experience.html');
+		} else if (target && target.classList.contains('skills')) {
+			viewHtmlContent('../skills.html');
+		}
+	});
+
+	function checkHightContent(content) {
+		if (content.offsetHeight >= 834) {
+			content.style.paddingBottom = '30px';
+		} else {
+			content.style.paddingBottom = '0px';
+		}
+	}
+	checkHightContent(mainContent);
+
+});
+
+
